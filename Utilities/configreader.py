e73b95d484990d3fcb988db856582814556231e1
Akshay8147 <117804369+Akshay8147@users.noreply.github.com>
2025-07-14 12:38:42 +0530

    Delete Utilities directory

--- Utilities/configreader.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-
-def ConfigReader(section, Key):
-    config = ConfigParser()
-    config.read("C:\\Users\\BDDbehavePOM\\ConfigurationData\\config.ini")
-    return config.get(section, Key)
